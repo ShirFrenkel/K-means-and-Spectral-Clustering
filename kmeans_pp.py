b36bec843fcf1cac65db4b223b8e23c59fa5521f
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2021-02-28 12:05:29 +0200

    Update kmeans_pp.py
    changed func desc

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -50,14 +50,13 @@ def kmeans_pp_main(K, MAX_ITER, obs):
     :param MAX_ITER: the maximum number of iterations of the K-means algorithm, 0 < MAX_ITER
     :param obs: the observations (points) to be clustered, numpy matrix of shape (N,d)
     N = the number of observations, d = the dimension of each observation
-    :return: point_cluster_map
+    :return: point_cluster_map !!! or None if error occurred !!!
     point_cluster_map[i] = the index of the cluster that point i is belong to (count starts from 0)"""
     N = obs.shape[0]
     d = obs.shape[1]
     init_cent, init_indices = k_means_pp(obs, N, K, d)
     #print(','.join(str(x) for x in init_indices))  # DEL
     return ckm.api_func(init_cent, obs.tolist(), K, N, d, MAX_ITER)  # TODO, tom need to change C func to return point_cluster_map
-    # don't forget None case
 
 
 #kmeans_pp_main(3,300,np.array([[j for j in range(i,i+4)]for i in range(0,40,4)], np.float64))  # DEL
